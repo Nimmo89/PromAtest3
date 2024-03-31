ef0a06fa1bde7252ce860a9d08811a6ab4ed07d8
otp11 <otp1189@gmail.com>
2024-03-31 11:57:46 +0700

    initial

---
+++ public class AnimalRegistry {.java
@@ -0,0 +1,32 @@
+public class AnimalRegistry {
+    private static final String DB_URL = "jdbc:mysql://localhost/FriendsOfHumans";
+    private static final String USER = "kpa";
+    private static final String PASSWORD = "kpa11";
+
+    // Метод для добавления животного в базу данных
+    public void addAnimal(String animalType, String name, String command, String birthDate) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
+            String query = "INSERT INTO ";
+            if (animalType.equals("Domestic"))
+                query += "DomesticAnimals ";
+            else if (animalType.equals("Working"))
+                query += "WorkingAnimals ";
+            else {
+                System.out.println("Неизвестный тип животного.");
+                return;
+            }
+            query += "(animal_type, name, command, birth_date) VALUES (?, ?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, animalType);
+            preparedStatement.setString(2, name);
+            preparedStatement.setString(3, command);
+            preparedStatement.setString(4, birthDate);
+            preparedStatement.executeUpdate();
+            System.out.println("Животное добалено.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Другие методы для работы с реестром
+}
---
+++ public class Counter implements AutoClos.java
@@ -0,0 +1,18 @@
+public class Counter implements AutoCloseable {
+    private int count;
+
+    public Counter() {
+        this.count = 0;
+    }
+
+    public void add() {
+        count++;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (count == 0) {
+            throw new Exception("Ресурс не используется.");
+        }
+    }
+}
